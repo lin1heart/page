4e7bee73f7723248666114acc40f4bc2a664f04b
lin1heart <lin1heart@gmail.com>
2018-01-02 15:13:36 +0800

    img

--- src/js/list.js
+++ src/js/list.js
@@ -1,17 +1,17 @@
 $(function(){
-  var da = getList('2','1','2');
-  if(da!="error"){
-      show();
-  }else{
-      alert(da);
-  }
+//var da = getList('2','1','2');
+//if(da!="error"){
+//    show();
+//}else{
+//    alert(da);
+//}
+	var da = [{"name":"aaa","id":"1","url":""},]
+	image_val(da);
 });
 var wid = '300px';
 var hei = '400px';
 var app =  new Vue({
-    el: '#app',
-    methods: {
-    }
+    el: '#app'
 });
 function show() {
     var dir = document.getElementById('conntion');
@@ -20,4 +20,21 @@ function show() {
 
     dd.className = "box";
 
+}
+var img_width = [];
+var img_height = [];
+var i = 0;
+//图片校验
+function image_val(da) {
+		var url = "http://ws.changxing.sh.cn:10180/ws/file/download/5a17ebc09536030bd409193c";
+		for(var j = 0; j<da.length ; j++){
+			
+		}
+    var image = new Image();  
+    image.src = url;
+    image.onload = function() {
+    	img_width[i] = image.width;
+    	img_height[i] = image.height;
+    	i++;
+    }
 }
