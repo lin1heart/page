1b161b5b1a5e356263e7645b9d3e68dc1806ea17
lin1heart <lin1heart@gmail.com>
2018-01-02 21:29:51 +0800

    img check ok

--- src/js/list.js
+++ src/js/list.js
@@ -5,36 +5,63 @@ $(function(){
 //}else{
 //    alert(da);
 //}
-	var da = [{"name":"aaa","id":"1","url":""},]
-	image_val(da);
+	var da = [];
+    height_i = image_val(da);
+    show();
 });
-var wid = '300px';
-var hei = '400px';
+var height_i;
 var app =  new Vue({
     el: '#app'
 });
+
+var img_url = ["../img/1.jpg","../img/3_4.png","../img/6.jpg","../img/7.jpg"];
 function show() {
-    var dir = document.getElementById('conntion');
-    var dbox = document.createElement('div');//第一行的div
-    var dr = document.createElement('div');
-
-    dd.className = "box";
-
+    var check = document.getElementById('dbox');
+    if(check){
+        check.parentNode.removeChild(check);
+    }else{
+        var dir = document.getElementById('conntion');
+        var dbox = document.createElement('div');//第一行的div
+        dbox.className = "box";
+        dbox.id = "dbox";
+        for(var x = 0;x<4; x++){
+            var dr = document.createElement('div');
+            dr.className = "img_div";
+            dr.style.height = height_i+'px';
+            dr.id = "dr" + x;
+            var img = document.createElement('img');
+            img.src = img_url[x];
+            dr.appendChild(img);
+            dbox.appendChild(dr);
+        }
+        dir.append(dbox);
+    }
 }
-var img_width = [];
-var img_height = [];
+var img_pro = [];
 var i = 0;
 //图片校验
 function image_val(da) {
 		var url = "http://ws.changxing.sh.cn:10180/ws/file/download/5a17ebc09536030bd409193c";
 		for(var j = 0; j<da.length ; j++){
-			
-		}
-    var image = new Image();  
-    image.src = url;
-    image.onload = function() {
-    	img_width[i] = image.width;
-    	img_height[i] = image.height;
-    	i++;
-    }
+            var image = new Image();  
+            image.src = da[j].url;
+            image.onload = function() {
+                img_pro[i] = image.width/image.height;
+                i++;
+            }
+        }
+        img_pro = [1366/768,162/270,1366/768,1366/768];
+        var wid = 0;
+        var body_width =  document.body.clientWidth - 40;
+        for(var a = 0; a< img_pro.length; a++){
+            wid += img_pro[a];
+        }
+        var heig = body_width/wid;
+        return heig; 
+}
+// 页面改变 重新绘制
+window.onresize = function(){
+    var da = [];
+    height_i = image_val(da);
+    show();
 }
